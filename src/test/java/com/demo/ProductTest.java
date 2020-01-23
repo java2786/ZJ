c66c2ffdf6131fad0804de0ed8e728c60e6a8e8b
Arun Kumar <java.kumar.arun@gmail.com>
2020-01-23 12:37:28 +0530

    test cases for product added

---
+++ src/test/java/com/demo/ProductTest.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ProductTest 
+{
+    @Test
+    public void shouldReturn()
+    {
+    	// selenium
+    	// cucumber
+    	// spring test
+    	// api
+        Assert.assertTrue( true );
+    }
+    @Test
+    public void shouldReplace()
+    {
+        Assert.assertFalse( false );
+    }
+    @Test
+    public void shouldBuy()
+    {
+        Assert.assertEquals(5, 6);
+    }
+}
